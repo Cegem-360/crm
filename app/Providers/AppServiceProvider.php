1caae943968b4f210aab11c5ffd417de44338486
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2026-01-22 13:56:06 +0100

    refactor: Remove unused Livewire support file upload namespace and related code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,11 +13,8 @@ use App\Models\Interaction;
 use App\Models\Opportunity;
 use App\Models\User;
 use Filament\Commands\FileGenerators\Resources\ResourceClassGenerator as BaseResourceClassGenerator;
-use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentTimezone;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -37,9 +34,6 @@ final class AppServiceProvider extends ServiceProvider
     {
         FilamentTimezone::set('Europe/Budapest');
 
-        FilamentAsset::register([
-            Js::make('chart-js-plugins', Vite::asset('resources/js/filament-chart-js-plugins.js'))->module(),
-        ]);
         $this->app->bind(BaseResourceClassGenerator::class, ResourceClassGenerator::class);
         Relation::enforceMorphMap([
             'bug_report' => BugReport::class,
